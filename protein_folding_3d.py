01e4545a987b8ef7a881ebdacfe895340d6b336c
Marek Rychlik <rychlik@arizona.edu>
2025-01-20 08:45:49 -0700

    *** empty log message ***

--- protein_folding_3d.py
+++ protein_folding_3d.py
@@ -73,7 +73,7 @@ def optimize_protein(positions, n_beads, write_csv=False):
     if write_csv:
         csv_filepath = f'protein{n_beads}.csv'
         np.savetxt(csv_filepath, trajectory[-1], delimiter=",")
-        
+
     return result, trajectory
 
 # 3D visualization function
